c931d7ff2c29321ac974c49fd70bf811ffb29346
arpit5492 <arpitmallick99@gmail.com>
2024-05-09 05:03:06 -0700

    Updated terminal UI

---
+++ lua/arpit/core/init.lua
@@ -0,0 +1,3 @@
+require("arpit.core.keymaps")
+require("arpit.core.options")
+
--- lua/arpit/core/options.lua
+++ lua/arpit/core/options.lua
@@ -1,3 +1,5 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
 local opt = vim.opt -- for conciseness
 
 -- line numbers
@@ -10,17 +12,16 @@ opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
--- line wrapping
-opt.wrap = false -- disable line wrapping
+opt.wrap = false
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
--- cursor line
-opt.cursorline = true -- highlight the current cursor line
+opt.cursorline = true
 
--- turn on termguicolors for nightfly colorscheme to work
+-- turn on termguicolors for tokyonight colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
@@ -35,6 +36,5 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
-opt.iskeyword:append("-") -- consider string-string as whole word
-
-
+-- turn off swapfile
+opt.swapfile = false
