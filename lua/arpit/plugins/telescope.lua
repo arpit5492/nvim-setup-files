5c04f760b61a14ee09ce810b8ed73f87beb6c404
arpit5492 <arpitmallick99@gmail.com>
2024-05-09 15:56:44 -0700

    added telescope configs

--- lua/arpit/plugins/telescope.lua
+++ lua/arpit/plugins/telescope.lua
@@ -1,32 +1,61 @@
--- import telescope plugin safely
-local telescope_setup, telescope = pcall(require, "telescope")
-if not telescope_setup then
-  return
-end
-
--- import telescope actions safely
-local actions_setup, actions = pcall(require, "telescope.actions")
-if not actions_setup then
-  return
-end
-
--- configure telescope
-telescope.setup({
-  -- configure custom mappings
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-        ["<C-j>"] = actions.move_selection_next, -- move to next result
-        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
-      },
-    },
-    layout_config = {
-      horizontal = {
-        preview_cutoff = 0,
-      },
-    },
+return {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+    "folke/todo-comments.nvim",
+    "folke/trouble.nvim", -- Ensure `trouble.nvim` is listed as a dependency
   },
-})
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local transform_mod = require("telescope.actions.mt").transform_mod
 
-telescope.load_extension("fzf")
+    local trouble = require("trouble")
+    local trouble_telescope = require("trouble.providers.telescope")
+
+    -- Custom action for integrating Trouble with Telescope
+    local custom_actions = transform_mod({
+      open_trouble_qflist = function(prompt_bufnr)
+        trouble.toggle("quickfix")
+      end,
+    })
+
+    -- Set up Trouble
+    trouble.setup({
+      -- You can define your custom trouble settings here
+    })
+
+    -- Set up Telescope with Trouble integration
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-q>"] = function(prompt_bufnr)
+              actions.send_selected_to_qflist(prompt_bufnr)
+              custom_actions.open_trouble_qflist(prompt_bufnr)
+            end,
+            ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
+          },
+        },
+      },
+    })
+
+    -- Load additional extensions
+    telescope.load_extension("fzf")
+
+    -- Set keymaps
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+  end,
+}
+
